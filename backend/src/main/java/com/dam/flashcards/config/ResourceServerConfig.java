51bd66a36ff0120f358a9af27b3effd9298a69a6
Nilton da Silva Junior <tonsti@gmail.com>
2021-12-18 20:21:02 +0100

    Habilitando acceso a consola de H2 en entorno de test

--- backend/src/main/java/com/dam/flashcards/config/ResourceServerConfig.java
+++ backend/src/main/java/com/dam/flashcards/config/ResourceServerConfig.java
@@ -1,7 +1,10 @@
 package com.dam.flashcards.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
@@ -13,9 +16,12 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
 	@Autowired
+	private Environment env;
+
+	@Autowired
 	private JwtTokenStore tokenStore;
 
-	private static final String[] PUBLIC = { "oauth/token" };
+	private static final String[] PUBLIC = { "oauth/token", "/h2-console/**" };
 
 	private static final String[] USUARIO_O_ADMINISTRADOR = { "/tarjetas/**", "/categorias/**" };
 
@@ -28,6 +34,11 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
+		// para liberar acceso a la consola de H2 en test
+		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable();
+		}
+
 		http.authorizeRequests().antMatchers(PUBLIC).permitAll().antMatchers(USUARIO_O_ADMINISTRADOR).permitAll()
 				.antMatchers(USUARIO_O_ADMINISTRADOR).hasAnyRole("USUARIO", "ADMINISTRADOR").antMatchers(ADMINISTRADOR)
 				.hasRole("ADMINISTRADOR").anyRequest().authenticated();
