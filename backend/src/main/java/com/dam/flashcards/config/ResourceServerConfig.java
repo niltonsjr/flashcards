ed2312a24de0565d4fa512d12c9e8006be2a0e3f
Nilton da Silva Junior <tonsti@gmail.com>
2021-12-31 12:02:17 +0100

    Adicionando configuración de CORS

--- backend/src/main/java/com/dam/flashcards/config/ResourceServerConfig.java
+++ backend/src/main/java/com/dam/flashcards/config/ResourceServerConfig.java
@@ -3,7 +3,10 @@ package com.dam.flashcards.config;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,6 +14,10 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableResourceServer
@@ -25,7 +32,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	private static final String[] PUBLIC = { "oauth/token", "/h2-console/**" };
 
 	private static final String[] USUARIO_O_ADMINISTRADOR = { "/tarjetas/**", "/categorias/**" };
-	
+
 	private static final String[] USUARIOS = { "/usuarios/**" };
 
 	private static final String[] ADMINISTRADOR = { "/roles/**" };
@@ -43,11 +50,34 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 		}
 
 		http.authorizeRequests().antMatchers(PUBLIC).permitAll()
-		.antMatchers(USUARIO_O_ADMINISTRADOR).hasAnyRole("USUARIO", "ADMINISTRADOR")
-		.antMatchers(HttpMethod.GET, USUARIOS).hasAnyRole("USUARIO")
-		.antMatchers(USUARIOS).hasAnyRole("ADMINISTRADOR")
-		.antMatchers(ADMINISTRADOR).hasRole("ADMINISTRADOR")
-		.anyRequest().authenticated();
+				.antMatchers(USUARIO_O_ADMINISTRADOR).hasAnyRole("USUARIO", "ADMINISTRADOR")
+				.antMatchers(HttpMethod.GET, USUARIOS).hasAnyRole("USUARIO")
+				.antMatchers(USUARIOS).hasAnyRole("ADMINISTRADOR")
+				.antMatchers(ADMINISTRADOR).hasRole("ADMINISTRADOR")
+				.anyRequest().authenticated();
+
+				http.cors().configurationSource(corsConfigurationSource());
+	}
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration corsConfig = new CorsConfiguration();
+		corsConfig.setAllowedOriginPatterns(Arrays.asList("*"));
+		corsConfig.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "PATCH"));
+		corsConfig.setAllowCredentials(true);
+		corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", corsConfig);
+		return source;
+	}
+
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter() {
+		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(
+				new CorsFilter(corsConfigurationSource()));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;
 	}
 
 }
