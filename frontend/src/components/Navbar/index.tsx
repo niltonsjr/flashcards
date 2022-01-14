f9f2b7dbca927fba01612455efc23d28c6142d02
Nilton da Silva Junior <tonsti@gmail.com>
2022-01-14 21:11:37 +0100

    Estilización de la barra de navegación superior

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,16 +1,33 @@
 import "./styles.css";
+import "bootstrap/js/src/collapse.js";
+import { ReactComponent as Logo } from "./../../assets/images/logo.svg";
 
 const Navbar = () => {
   return (
-    <nav className="bg-primary">
-      <div>
-        <a href="link">
+    <nav className="navbar navbar-expand-md navbar-light main-nav">
+      <div className="container-fluid">
+        <a href="link" className="nav-logo-text">
+          <Logo className="nav-logo" />
           <h4>FlashCards</h4>
         </a>
-        <div>
-          <ul>
+        <button
+          className="navbar-toggler custom-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#flashcards-navbar"
+          aria-controls="flashcards-navbar"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="flashcards-navbar">
+          <ul className="navbar-nav offset-md-2 main-menu">
             <li>
-              <a href="link">Administrar</a>
+              <a href="link" className="active">
+                Administrar
+              </a>
             </li>
             <li>
               <a href="link">Estudiar</a>
