8183f099cf1cdc6c5cfa1fe9e775bcfbf73f68f0
Nilton da Silva Junior <tonsti@gmail.com>
2022-01-16 14:14:26 +0100

    Estilizando home background y login card

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,11 +1,16 @@
-import Navbar from "components/Navbar";
 import { ReactComponent as LargeImage } from "assets/images/home-image-large.svg";
-//import { ReactComponent as SmallImage } from "assets/images/home-image-small.svg";
-//import largeBackground from 'assets/images/background-large.svg';
+import { ReactComponent as SmallImage } from "assets/images/home-image-small.svg";
 import "./styles.css";
 import LoginCard from "components/LoginCard";
 
 const Home = () => {
+  const mql = window.matchMedia("(max-width: 576px)");
+  let mobileView;
+
+  mql.addEventListener("change", (e) => {
+    mobileView = e.matches;
+  });
+
   return (
     <div className="home-container">
       <div className="home-left-container">
@@ -14,7 +19,7 @@ const Home = () => {
           para ayudarte a retener información <br /> en menos tiempo y accede a
           ellas <br /> desde cualquier lugar.
         </h1>
-        <LargeImage />
+        {mobileView ? <SmallImage /> : <LargeImage /> }
       </div>
       <div className="home-right-container">
         <LoginCard />
