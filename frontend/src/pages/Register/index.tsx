59f1d62a7e45bec22d6c96712732f0c2e1ed9cbb
Nilton da Silva Junior <tonsti@gmail.com>
2022-01-17 21:55:28 +0100

    Ajustando layout de formulario de registro hasta width 768px

--- frontend/src/pages/Register/index.tsx
+++ frontend/src/pages/Register/index.tsx
@@ -1,15 +1,19 @@
 import { Link } from "react-router-dom";
+import linea from "assets/images/line-small.svg";
 import "./styles.css";
 
 const Register = () => {
   return (
     <div className="register-container">
-      <h1>
-        Regístrate para empezar a <br /> crear tus tarjetas
-      </h1>
+      <div className="register-header">
+        <img src={linea} alt="linea" />
+        <h1>Regístrate para empezar a crear tus tarjetas</h1>
+        <img src={linea} alt="linea" />
+      </div>
+      <hr />
       <div className="register-form-container">
-        <form>
-          <div className="mb-3">
+        <form className="row row-cols-lg-2 g-3">
+          <div className="mb-3 col-12">
             <label htmlFor="nombreUsuario" className="form-label">
               Nombre de usuario:
             </label>
@@ -20,51 +24,55 @@ const Register = () => {
               name="nombreUsuario"
             />
           </div>
-          <div className="mb-3">
-            <label htmlFor="contrasena" className="form-label">
-              Contraseña:
-            </label>
-            <input
-              type="password"
-              id="contrasena"
-              className="form-control base-input"
-              name="contrasena"
-            />
+          <div className="passwords-container col-12">
+            <div className="mb-3">
+              <label htmlFor="contrasena" className="form-label">
+                Contraseña:
+              </label>
+              <input
+                type="password"
+                id="contrasena"
+                className="form-control base-input"
+                name="contrasena"
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="confirmContrasena" className="form-label">
+                Confirmar contraseña:
+              </label>
+              <input
+                type="password"
+                id="confirmContrasena"
+                className="form-control base-input"
+                name="confirmContrasena"
+              />
+            </div>
           </div>
-          <div className="mb-3">
-            <label htmlFor="confirmContrasena" className="form-label">
-              Confirmar contraseña:
-            </label>
-            <input
-              type="password"
-              id="confirmContrasena"
-              className="form-control base-input"
-              name="confirmContrasena"
-            />
+          <div className="fullname-container col-12">
+            <div className="mb-3">
+              <label htmlFor="nombre" className="form-label">
+                Nombre:
+              </label>
+              <input
+                type="text"
+                id="nombre"
+                className="form-control base-input"
+                name="nombre"
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="apellidos" className="form-label">
+                Apellidos:
+              </label>
+              <input
+                type="text"
+                id="apellidos"
+                className="form-control base-input"
+                name="apellidos"
+              />
+            </div>
           </div>
-          <div className="mb-3">
-            <label htmlFor="nombre" className="form-label">
-              Nombre:
-            </label>
-            <input
-              type="text"
-              id="nombre"
-              className="form-control base-input"
-              name="nombre"
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="apellidos" className="form-label">
-              Apellidos:
-            </label>
-            <input
-              type="text"
-              id="apellidos"
-              className="form-control base-input"
-              name="apellidos"
-            />
-          </div>
-          <div className="mb-3">
+          <div className="mb-3 col-12">
             <label htmlFor="email" className="form-label">
               Correo electrónico:
             </label>
@@ -75,20 +83,26 @@ const Register = () => {
               name="email"
             />
           </div>
-          <div className="form-check">
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value=""
-              id="flexCheckDefault"
-            />
-            <label className="form-check-label" htmlFor="flexCheckDefault">
-              Acepto los <Link to="/condiciones" className="conditions-link-register">Términos y Condiciones</Link> y doy mi consentimiento para el
-              uso de datos tal y como se detalla en la Política de Privacidad.
-            </label>
-          </div>
-          <div>
-            <button className="register-submit">Aceptar</button>
+          <div className="register-buttom-container col-12">
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value=""
+                id="flexCheckDefault"
+              />
+              <label className="form-check-label" htmlFor="flexCheckDefault">
+                Acepto los
+                <Link to="/condiciones" className="conditions-link-register">
+                  Términos y Condiciones
+                </Link>
+                y doy mi consentimiento para el uso de datos tal y como se
+                detalla en la Política de Privacidad.
+              </label>
+            </div>
+            <div>
+              <button className="register-submit">Aceptar</button>
+            </div>
           </div>
         </form>
       </div>
