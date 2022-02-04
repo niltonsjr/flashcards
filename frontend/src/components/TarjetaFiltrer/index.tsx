64e797407f0f57ba96882919c976a4e9353e307f
Nilton da Silva Junior <tonsti@gmail.com>
2022-02-04 21:35:27 +0100

    Iniciando estilización del componente TarjetaFilter

--- frontend/src/components/TarjetaFiltrer/index.tsx
+++ frontend/src/components/TarjetaFiltrer/index.tsx
@@ -1,33 +1,74 @@
-import { Link } from "react-router-dom";
 import { ReactComponent as SearchIcon } from "assets/images/search-icon.svg";
+import { AxiosRequestConfig } from "axios";
+import { useEffect, useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+import Select from "react-select";
+import { Categoria } from "types/categoria";
+import { requestBackend } from "util/requests";
 import "./styles.css";
 
+type TarjetaFilterData = {
+  texto: string;
+  categoria: Categoria;
+};
+
 const TarjetaFilter = () => {
+  const [selectCategorias, setSelectCategorias] = useState<Categoria[]>([]);
+
+  useEffect(() => {
+    const config: AxiosRequestConfig = {
+      method: "GET",
+      url: "/categorias",
+      withCredentials: true,
+    };
+    requestBackend(config).then((response) => {
+      setSelectCategorias(response.data.content);
+    });
+  }, []);
+
+  const { register, handleSubmit, control } = useForm<TarjetaFilterData>();
+
+  const onSubmit = (formData: TarjetaFilterData) => {
+    console.log("enviou", formData);
+  };
+
   return (
     <div className="tarjeta-filter-crud-container base-card">
-      <form action="" className="tarjeta-filter-form">
-        <div className="tarjeta-filter-text-container">
+      <form onSubmit={handleSubmit(onSubmit)} className="tarjeta-filter-form">
+        <div className="tarjeta-filter-text-container bg-white">
           <input
+            {...register("texto")}
             type="text"
+            name="texto"
             className="form-control base-input bg-white"
             placeholder="Buscar por texto"
           />
-          <SearchIcon />
+          <button className="tarjeta-filter-search-icon bg-white">
+            <SearchIcon />
+          </button>
         </div>
         <div className="tarjeta-filter-bottom-container">
           <div className="tarjeta-filter-categoria-contaier">
-            <select
-              className="form-select tarjeta-filter-crud-select bg-white"
-              aria-label="Default select example"
-              defaultValue={"categoria"}
-            >
-              <option value="categoria">Categoría</option>
-              <option value="1">One</option>
-              <option value="2">Two</option>
-              <option value="3">Three</option>
-            </select>
+            <Controller
+              name="categoria"
+              control={control}
+              render={({ field }) => (
+                <Select
+                  {...field}
+                  classNamePrefix="tarjeta-filter-select"
+                  options={selectCategorias}
+                  getOptionLabel={(cat: Categoria) => cat.nombre}
+                  getOptionValue={(cat: Categoria) => String(cat.id)}
+                  placeholder="Categoría"
+                  isClearable
+                />
+              )}
+            />
           </div>
-          <button className="btn btn-outline-secondary">Limpiar</button>
+          <button className="btn btn-outline-secondary">
+            Limpiar <span className="btn-tarjeta-filter-word">filtro</span>
+          </button>
         </div>
       </form>
       <Link to="/admin/tarjetas/nueva">
