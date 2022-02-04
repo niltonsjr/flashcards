259211fba6f7cb4f370a2c97540e44af39533afb
Nilton da Silva Junior <tonsti@gmail.com>
2022-02-04 19:54:12 +0100

    Refactorizando método de getTarjetas en componente List para utilizar hook useCallback

--- frontend/src/pages/Admin/Tarjetas/List/index.tsx
+++ frontend/src/pages/Admin/Tarjetas/List/index.tsx
@@ -1,33 +1,46 @@
 import { AxiosRequestConfig } from "axios";
 import Pagination from "components/Pagination";
 import TarjetaCard from "components/TarjetaCard";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { SpringPage } from "types/spring";
 import { Tarjeta } from "types/tarjeta";
 import { requestBackend } from "util/requests";
 import "./styles.css";
 
+type ControlComponentsData = {
+  activePage: number;
+};
+
 const List = () => {
   const [page, setPage] = useState<SpringPage<Tarjeta>>();
-  useEffect(() => {
-    getTarjetas(0);
-  }, []);
+  const [controlComponentsData, setControlComponentsData] =
+    useState<ControlComponentsData>({
+      activePage: 0,
+    });
 
-  const getTarjetas = (pageNumber: number) => {
+  const getTarjetas = useCallback(() => {
     const config: AxiosRequestConfig = {
       method: "GET",
       url: "/tarjetas",
       withCredentials: true,
       params: {
-        page: pageNumber,
-        size: 5,
+        page: controlComponentsData.activePage,
+        size: 3,
       },
     };
 
     requestBackend(config).then((response) => {
       setPage(response.data);
     });
+  }, [controlComponentsData]);
+
+  useEffect(() => {
+    getTarjetas();
+  }, [getTarjetas]);
+
+  const handlePageChange = (pageNumber: number) => {
+    setControlComponentsData({ activePage: pageNumber });
   };
 
   return (
@@ -54,17 +67,14 @@ const List = () => {
       <div className="tarjetas-list-container">
         {page?.content.map((tarjeta) => (
           <div key={tarjeta.id}>
-            <TarjetaCard
-              tarjeta={tarjeta}
-              onDelete={() => getTarjetas(page.number)}
-            />
+            <TarjetaCard tarjeta={tarjeta} onDelete={getTarjetas} />
           </div>
         ))}
       </div>
       <Pagination
         pageCount={page ? page?.totalPages : 0}
-        range={3}
-        onChange={getTarjetas}
+        range={2}
+        onChange={handlePageChange}
       />
     </>
   );
