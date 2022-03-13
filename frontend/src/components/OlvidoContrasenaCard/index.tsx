a520e74281c4033433213f097648e9e66e4d2600
Nilton da Silva Junior <tonsti@gmail.com>
2022-03-13 13:37:03 +0100

    Reformulando tarjeta de recuperar contraseña y cambiando estilo de fondo de pantalla de registro para que coincida con la de login

--- frontend/src/components/OlvidoContrasenaCard/index.tsx
+++ frontend/src/components/OlvidoContrasenaCard/index.tsx
@@ -1,8 +1,7 @@
 import { AxiosRequestConfig } from "axios";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
-import { toast } from "react-toastify";
+import { Link, useNavigate } from "react-router-dom";
 import { requestBackend } from "util/requests";
 import "./styles.css";
 
@@ -16,6 +15,7 @@ const OlvidoContrasenaCard = () => {
     handleSubmit,
     formState: { errors },
   } = useForm<CorreoDTO>();
+  const [enviado, setEnviado] = useState<boolean>(false);
   const navigate = useNavigate();
 
   const onSubmit = (formData: CorreoDTO) => {
@@ -28,49 +28,75 @@ const OlvidoContrasenaCard = () => {
 
     requestBackend(config)
       .then((response) => {
-        console.log("Suceso", response);
-        toast.success("Usuario creado correctamente.");
-        navigate("/auth/login");
+        setEnviado(true);
       })
-      .catch((error) => {
-        console.log("Erro:", error);
-        toast.error(`Error al crear el usuario: ${error.message}`);
+      .catch(() => {
+        setEnviado(true);
       });
+  };
+
+  const handleClick = () => {
+    navigate("/auth/login");
   };
 
   return (
     <div className="olvido-contrasena-card">
       <h1>Recuperar contraseña</h1>
-      <p>
-        Introduzca su correo electrónico abajo y le enviaremos un correo con las
-        instrucciones para resetear su contraseña.
-      </p>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="mb-4">
-          <input
-            {...register("email", {
-              required: "Campo obligatorio",
-            })}
-            type="text"
-            className={`form-control base-input ${
-              errors.email ? "is-invalid" : ""
-            }`}
-            placeholder="Email"
-            name="email"
-          />
-          <div className="invalid-feedback d-block">
-            {errors.email?.message}
+      {!enviado ? (
+        <>
+          <p>
+            Introduzca su correo electrónico abajo y le enviaremos un correo con
+            las instrucciones para resetear su contraseña.
+          </p>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="mb-4">
+              <input
+                {...register("email", {
+                  required: "Campo obligatorio",
+                })}
+                type="text"
+                className={`form-control base-input ${
+                  errors.email ? "is-invalid" : ""
+                }`}
+                placeholder="Email"
+                name="email"
+              />
+              <div className="invalid-feedback d-block">
+                {errors.email?.message}
+              </div>
+            </div>
+            <Link to="/auth/login" className="login-link-recover">
+              Volver al Login
+            </Link>
+            <div>
+              <button type="submit" className="olvido-contrasena-submit">
+                Aceptar
+              </button>
+            </div>
+          </form>
+        </>
+      ) : (
+        <>
+          <p>
+            Si el correo electrónico facilitado se encuentra registrado,
+            recibirá un correo con instrucciones para resetear su contraseña. Si
+            no ha recibido el correo, deberá crear una cuenta{" "}
+            <Link to="/auth/register" className="link-primary">
+              aquí
+            </Link>
+            .
+          </p>
+          <div>
+            <button
+              type="button"
+              className="olvido-contrasena-submit"
+              onClick={handleClick}
+            >
+              Volver al Login
+            </button>
           </div>
-        </div>
-        <Link to="/auth/login" className="login-link-recover">
-          Volver al login
-        </Link>
-        <div>
-          <button type="submit" className="login-submit">
-            Aceptar
-          </button>
-        </div>
-      </form>
+        </>
+      )}
     </div>
   );
 };
