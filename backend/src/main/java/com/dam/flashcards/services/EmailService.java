08d9157d60e813a4ba99a7873bd06b1f71b5660b
Nilton da Silva Junior <tonsti@gmail.com>
2022-04-19 22:10:12 +0200

    Adicionando emailService

--- backend/src/main/java/com/dam/flashcards/services/EmailService.java
+++ backend/src/main/java/com/dam/flashcards/services/EmailService.java
@@ -2,6 +2,8 @@ package com.dam.flashcards.services;
 
 import com.dam.flashcards.dto.EmailDTO;
 
+import org.springframework.stereotype.Service;
+@Service
 public interface EmailService {
 
     void sendEmail(EmailDTO dto);
