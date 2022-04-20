4781ae0b6dc9d59f6deeeffb10857955a4fb0e31
Nilton da Silva Junior <tonsti@gmail.com>
2022-04-20 21:00:34 +0200

    Adicionando archivo de configuración para seleccionar implementación de emailService en profile prod

---
+++ backend/src/main/java/com/dam/flashcards/config/ProdConfig.java
@@ -0,0 +1,18 @@
+package com.dam.flashcards.config;
+
+import com.dam.flashcards.services.EmailService;
+import com.dam.flashcards.services.SendGridEmailService;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("prod")
+public class ProdConfig {
+
+    @Bean
+    public EmailService emailService() {
+        return new SendGridEmailService();
+    }
+}
--- backend/src/main/java/com/dam/flashcards/services/EmailService.java
+++ backend/src/main/java/com/dam/flashcards/services/EmailService.java
@@ -2,8 +2,6 @@ package com.dam.flashcards.services;
 
 import com.dam.flashcards.dto.EmailDTO;
 
-import org.springframework.stereotype.Service;
-@Service
 public interface EmailService {
 
     void sendEmail(EmailDTO dto);
